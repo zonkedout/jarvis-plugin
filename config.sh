e25375a204e38b15a35ba0e97ea8ac74f41bb528
Matt <matt@zonkedout.org>
2016-11-30 19:52:20 +0100

    update

---
+++ config.sh
@@ -0,0 +1,18 @@
+#API KEY citymapper  : https://citymapper.3scale.net
+APIKEYCM=
+
+#coordonnees du point de depart LATITUDE,LONGITUDE avec "Pont Notre Dame, 75004 Paris, France"
+#voir http://www.gpsfrance.net/adresse-vers-coordonnees-gps
+FROM=48.856145,2.348601
+
+#coordonnees destination
+chezqui='{ "devices":[
+{ "latitude": "48.833999", "longitude": "2.339946", "nomdestination": "jacques"},
+{ "latitude": "48.860024", "longitude": "2.390866", "nomdestination": "papi"},
+{ "latitude": "48.869056", "longitude": "2.309932", "nomdestination": "pierre"}
+]}'
+
+#Autre
+DATE="$(date +"%Y-%m-%dT%H:%M:%S%z")"
+
+
